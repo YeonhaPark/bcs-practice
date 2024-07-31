94b8a16d34c6cb4550ad75380c53bd2daa860ca6
YeonhaPark <yonaprisca@gmail.com>
2024-07-31 14:33:29 +0900

    feedback: coding test 8(Vote)

--- coding-test/8.sol
+++ coding-test/8.sol
@@ -17,9 +17,11 @@ pragma solidity ^0.8.19;
 * 전체 안건 확인 기능 - 제목으로 안건을 검색하면 번호, 제목, 내용, 제안자, 찬반 수 모두를 반환하는 기능
 -------------------------------------------------------------------------------------------------
 * 안건 진행 과정 - 투표 진행중, 통과, 기각 상태를 구별하여 알려주고 
-전체의 70%가 투표에 참여하고 투표자의 66% 이상이 찬성해야 통과로 변경, 둘 중 하나라도 만족못하면 기각 
+전체의 70%가 투표에 참여하고 투표자의 66% 이상이 찬성해야 통과로 변경, 둘 중 하나라도 만족못하면 기각 => 70 % * 66 % = 47 명
  */
 
+
+// 토큰이 staking의 부분은 여기서 쳐내짐. 
 contract Reunion {
     struct Agenda {
         uint number;
@@ -28,58 +30,66 @@ contract Reunion {
         address raisedBy;
         uint supporterVotes;
         uint opponentVotes;
+        status result;
+        uint blockNumber;
     }
 
     struct Voter {
         string name;
         address addr;
-        Agenda[] agendas;
-        Agenda[] votedFor;
+        string[] agendas;
         mapping(string => bool) voteResult;
+        mapping(string => bool) voted;
     }
-    Agenda[] allAgendas;
-    mapping(string => Agenda) agendaByTitle;
+    mapping(string => Agenda) public agendaByTitle;
     mapping(address => Agenda) agendaByAddr;
     mapping(address => Agenda[]) agendasByAddr;
     mapping(address => Voter) voters;
-    enum Status {
-        ONGOING,
-        PASSED,
-        DISMISSED
+    uint voterCount;
+    uint agendaCount = 1;
+    enum status {
+        ongoing,
+        passed,
+        failed
     }
 
     // 사용자 등록 기능 - 사용자를 등록하는 기능
-    function registerVoter(string memory name, address addr) public {
-        voters[addr].name = name;
-        voters[addr].addr = addr;
+    function registerVoter(string calldata name) public {
+        require(voters[msg.sender].addr == address(0));
+        voters[msg.sender].name = name;
+        voters[msg.sender].addr = msg.sender;
+        voterCount++;
     }
 
 
     // 전체 안건 확인 기능 - 제목으로 안건을 검색하면 번호, 제목, 내용, 제안자, 찬반 수 모두를 반환하는 기능
-    function getAgenda(string memory title) public view returns(Agenda memory) {
-        return agendaByTitle[title];
+    function getAgenda(string calldata _title) public view returns(Agenda memory) {
+        return agendaByTitle[_title];
     }
     // 투표하는 기능 - 특정 안건에 대하여 투표하는 기능, 안건은 제목으로 검색, 이미 투표한 건에 대해서는 재투표 불가능
-    function voteFor(string memory title, bool vote) public view {
-        // 내가 투표한 아젠다 -> agendasByAddr[msg.sender].votedFor 가  agendaByTitle[title을 포함하면 안됨
-        bool alreadyVoted;
-        for (uint i = 0; i < agendasByAddr[msg.sender].length; i++) {
-            if (keccak256(abi.encodePacked(agendasByAddr[msg.sender][i].title)) == keccak256(abi.encodePacked(agendaByTitle[title].title))) {
-                alreadyVoted = true;
-                break;
-            }
+    function voteFor(string calldata _title, bool vote) public {
+        require(voters[msg.sender].addr != address(0));
+        require(voters[msg.sender].voteResult[_title] == false && agendaByTitle[_title].result == status.ongoing);
+        require(block.number <= agendaByTitle[_title].blockNumber + 15);
+        vote ? agendaByTitle[_title].supporterVotes++ : agendaByTitle[_title].opponentVotes++;
+        voters[msg.sender].voted[_title] = true;
+        voters[msg.sender].voteResult[_title] = vote;
+    }
+    // 전체의 70%가 투표에 참여하고 투표자의 66% 이상이 찬성해야 통과로 변경, 둘 중 하나라도 만족못하면 기각
+    function setResult(string calldata _title) public returns(status) {
+        require(agendaByTitle[_title].blockNumber + 15 <= block.number); // 실습 4번 web3.js
+        uint total = agendaByTitle[_title].supporterVotes + agendaByTitle[_title].opponentVotes;
+        uint pro =  agendaByTitle[_title].supporterVotes;
+        if (voterCount * 7 <= 10 * total &&  66 * total <= 100 * pro) {
+            return agendaByTitle[_title].result = status.passed;
+        } else {
+            return agendaByTitle[_title].result = status.failed;
         }
-        if (alreadyVoted) {
-            revert("You already voted for this agenda");
-        }
-        Agenda memory agendaToVote = getAgenda(title);
-        vote ? agendaToVote.supporterVotes++ : agendaToVote.opponentVotes++;
     }
     // 안건 제안 기능 - 자신이 원하는 안건을 제안하는 기능
-    function suggest(string memory title, string memory content) public {
-        uint agendaNumber = allAgendas.length;
-        Agenda memory newAgenda = Agenda(agendaNumber + 1, title, content, msg.sender, 0, 0);
-        allAgendas.push(newAgenda);
+    function suggest(string calldata _title, string calldata _content) public {
+        agendaByTitle[_title] = Agenda(agendaCount++, _title, _content, msg.sender, 0, 0, status.ongoing, block.number);
+        voters[msg.sender].agendas.push(_title);
     }
     struct AgendaDetail {
         uint number;
@@ -90,21 +100,30 @@ contract Reunion {
     }
     // 제안한 안건 확인 기능 - 자신이 제안한 안건에 대한 현재 진행 상황 확인기능 
     // (번호, 제목, 내용, 찬반 반환 // 밑의 심화 문제 풀었다면 상태도 반환)
-    function checkMyAgendaStatus(address myAddress) public view returns(AgendaDetail[] memory) {
-        Agenda[] memory myAgenda = agendasByAddr[myAddress];
-        AgendaDetail[] memory myAgendaDetails = new AgendaDetail[](myAgenda.length);
-        for (uint i = 0; i < myAgenda.length; i++) {
-            myAgendaDetails[i] = AgendaDetail(myAgenda[i].number, myAgenda[i].title, myAgenda[i].content, myAgenda[i].supporterVotes, myAgenda[i].opponentVotes);
+    function checkMyAgendaStatus() public view returns(Agenda[] memory) {
+        string[] memory _proposals = voters[msg.sender].agendas;
+        Agenda[] memory res = new Agenda[](_proposals.length);
+
+        for (uint i = 0; i < _proposals.length; i++) {
+            res[i] = agendaByTitle[_proposals[i]];
         } // 상태 추가하기
-        return myAgendaDetails;
+        return res;
     }
+}
 
-
-    // 안건 진행 과정 - 투표 진행중, 통과, 기각 상태를 구별하여 알려주고 
-    // 15개 블록 후에 전체의 70%가 투표에 참여하고 투표자의 66% 이상이 찬성해야 통과로 변경, 둘 중 하나라도 만족못하면 기각
-    function progress(string memory title) public view returns (Status){
-        Status currentStatus = Status.ONGOING;
-        return currentStatus;
+contract BLOCK {
+    constructor() {
+        blockNumber = block.number;  // 0 (contract creation) 1st block
     }
-
+    uint public blockNumber;
+    function getBlockHash(uint _n) public view returns(bytes32) {
+        return blockhash(_n);
+    }
+    function setBlockNumber() public {
+        blockNumber = block.number;
+    }
+    function getBlockNumber() public view returns(uint) {
+        return block.number; // 생성된 마지막 블록 넘버, 여기서 99를 뱉어낸다면, proposal의 시작점은 100으로 셋팅하는건 똑바로 된게 아니다. 
+        // 100을 뱉어낸다면 그냥 100으로 해야해. 
+    }
 }
